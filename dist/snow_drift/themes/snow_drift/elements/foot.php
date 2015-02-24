bbbc400f2467104f0a302722f1642ca23766cb79
Mike Lay <mike@mkly.io>
2015-02-23 23:47:41 -0600

    Add syntax

--- dist/snow_drift/themes/snow_drift/elements/foot.php
+++ dist/snow_drift/themes/snow_drift/elements/foot.php
@@ -2,3 +2,5 @@
 defined('C5_EXECUTE') or die('Access Denied.');
 
 Loader::element('footer_required');
+?>
+<script src="//cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/scripts/shAutoloader.js"></script>
