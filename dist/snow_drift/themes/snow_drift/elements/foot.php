a35d1b8cb619c9cc2ee0e5302d40de096ea2f68a
Mike Lay <mike@mkly.io>
2015-02-24 00:13:15 -0600

    Add css and autoload syntax

--- dist/snow_drift/themes/snow_drift/elements/foot.php
+++ dist/snow_drift/themes/snow_drift/elements/foot.php
@@ -3,4 +3,12 @@ defined('C5_EXECUTE') or die('Access Denied.');
 
 Loader::element('footer_required');
 ?>
-<script src="//cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/scripts/shAutoloader.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/scripts/shCore.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/scripts/shAutoloader.min.js"></script>
+<script>
+SyntaxHighlighter.autoloader(
+    'css //cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/scripts/shBrushCss.min.js',
+    'js //cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/scripts/shBrushJScript.min.js',
+    'php //cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/scripts/shBrushPhp.min.js'
+);
+</script>
--- dist/snow_drift/themes/snow_drift/elements/head.php
+++ dist/snow_drift/themes/snow_drift/elements/head.php
@@ -3,8 +3,9 @@
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='http://fonts.googleapis.com/css?family=PT+Sans:700,400|Vollkorn:400,400italic' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=PT+Sans:700,400|Vollkorn:400,400italic' rel='stylesheet' type='text/css'>
     <?= $html->css($view->getStylesheet('main.less')) ?>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/github.min.css" rel="stylesheet" type="text/css">
     <?= Loader::element('header_required', array('pageTitle' => $pageTitle)) ?>
     <script>window.SnowDrift = {};</script>
 </head>
