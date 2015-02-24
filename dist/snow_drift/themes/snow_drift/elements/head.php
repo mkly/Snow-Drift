c0c9570928f2de29799adeda82324d8f9f10e7fa
Mike Lay <mike@mkly.io>
2015-02-24 00:41:29 -0600

    Use correct syntax library

--- dist/snow_drift/themes/snow_drift/elements/head.php
+++ dist/snow_drift/themes/snow_drift/elements/head.php
@@ -6,8 +6,7 @@
     <link href='//fonts.googleapis.com/css?family=PT+Sans:700,400|Vollkorn:400,400italic' rel='stylesheet' type='text/css'>
     <?= $html->css($view->getStylesheet('main.less')) ?>
     <link href="//cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/styles/shCore.min.css" rel="stylesheet" type="text/css">
-    <link href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/github.min.css" rel="stylesheet" type="text/css">
-
+    <link href="//cdnjs.cloudflare.com/ajax/libs/SyntaxHighlighter/3.0.83/styles/shThemeEclipse.min.css" rel="stylesheet" type="text/css">
     <?= Loader::element('header_required', array('pageTitle' => $pageTitle)) ?>
     <script>window.SnowDrift = {};</script>
 </head>
